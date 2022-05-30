c0ed94551fc382b09b277238f5836ff2a96fe3e4
Hoan-K-Le <hoanleecr@gmail.com>
2022-05-30 14:53:07 -0700

    finished laying out the details page

--- server.js
+++ server.js
@@ -74,8 +74,13 @@ app.get('/anime', async (req,res) => {
     const searchURL = `https://api.jikan.moe/v4/anime?q=${req.query.animeSearch}&sfw`
     
    const response = await axios.get(searchURL)
-    console.log(response.data)
-    res.render('results.ejs', {anime: response.data.data})
+   const anime = response.data.data
+   // destructoring array into variables
+  //  const [a] = anime
+  //  console.log(a)
+
+    // console.log(response.data)
+    res.render('results.ejs', {anime})
     // res.send('hi')
 
   } catch(err) {
@@ -84,17 +89,24 @@ app.get('/anime', async (req,res) => {
   
 })
 
-app.get('/anime/detail', async (req,res) => {
+app.get('/anime/:id', async (req,res) => {
   try {
-    const idURL = `https://api.jikan.moe/v4/anime?q=${req.params.id}&sfw`
+    // console.log(req.params.id)
+    const idURL = `https://api.jikan.moe/v4/anime/${req.params.id}`
+    // const idURL = `https://api.jikan.moe/v4/anime?i=${req.params.id}`
     const response = await axios.get(idURL)
-    // console.log(response)
-    res.render('details.ejs', {anime: response.data.data})
+    const anime = response.data.data
+    // console.log(anime)
+    // const animeDetails = Object.entries()
+    // console.log(idURL)
+    res.render('details.ejs', {anime})
     // res.send('hi')
-  } catch {
+  } catch(err) {
     console.warn(err)
   }
 })
+
+
 
 
 // 404 ERROR HANDLER -- NEEDS TO GO LAST
@@ -103,10 +115,10 @@ app.get('/anime/detail', async (req,res) => {
 // }
 
 
-app.use((req,res,next) => {
-  // render a 404 template
-  res.status(404).render('404.ejs')
-})
+// app.use((req,res,next) => {
+//   // render a 404 template
+//   res.status(404).render('404.ejs')
+// })
 
 // 500 error handler
 // needs to have all 4 params
